5837a35195302078da3fa9b664d4996fc3214062
maximo jimeno <maximojimeno@gmail.com>
2020-09-13 17:00:23 -0400

    Controlador Persona: Se Agrego el Metodo Optener lista de Persona

--- Server/Controllers/PersonasController.cs
+++ Server/Controllers/PersonasController.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Persona.Server.Models;
 
 namespace Persona.Server.Controllers {
-    [Route("api/[controller]")]
+    [Route("api/persona")]
     [ApiController]
-    public class PersonasController : ControllerBase {
+    public class PersonasController : ControllerBase
+    {
 
+        private DbContexto _dbContexto;
+
+        public PersonasController(DbContexto dbContexto)
+        {
+            _dbContexto = dbContexto;
+        }
+
+        [HttpGet("")]
+
+        public ActionResult<IEnumerable<Shared.Persona>> GetPersonas()
+        {
+            return _dbContexto.personas.ToList();
+        }
     }
 }
