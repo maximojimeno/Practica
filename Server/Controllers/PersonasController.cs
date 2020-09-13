0afa50ed46616c092f5102760b00156b66920040
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2020-09-13 17:17:43 -0400

    merge

--- Server/Controllers/PersonasController.cs
+++ Server/Controllers/PersonasController.cs
@@ -4,11 +4,38 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persona.Server.Models;
 
 namespace Persona.Server.Controllers {
-    [Route("api/[controller]")]
+    [Route("api/personas")]
     [ApiController]
     public class PersonasController : ControllerBase {
 
+        private readonly DbContexto contexto;
+
+        public PersonasController(DbContexto _contexto) {
+            contexto = _contexto;
+        }
+
+
+        [HttpGet("{PersonaId}")]
+        public async Task<ActionResult<Shared.Persona>> GetPersona(int PersonaId) {
+
+            var persona = await contexto.personas.Where(p => p.id == PersonaId).SingleOrDefaultAsync();
+
+            if (persona == null) {
+                return NotFound();
+            } else {
+                return persona;
+            }
+
+
+        }
+
+
     }
+
+
+
 }
